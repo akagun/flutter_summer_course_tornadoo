c70fdd9edba657945a36cf7f490e3b92af01ec4d
Tornado-11 <tornado-11@Admin-11s-iMac.local>
2023-07-06 17:38:45 +0800

    exercise

---
+++ day_08/lib/exercise.dart
@@ -0,0 +1,21 @@
+void main (){
+  print('day 08');
+  print(average(3, 4));
+  print(multiply (1, 3, 5));
+  print("hello world");
+}
+int add(int a, int b) {
+  return a + b;
+}
+// exercise 01
+double average(int a, int b) {
+  return (a + b) / 2;
+}
+// exercise 02
+int multiply(int a, int b, int c) {
+  return a * b * c;
+}
+// exercise 03
+void connect(String a, String b) {
+  print("$a $b");
+}
