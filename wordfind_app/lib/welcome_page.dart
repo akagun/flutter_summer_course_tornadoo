b1dc1b24af768d0ab37e7a13bc5f3b6ec092fd85
Tornado-11 <tornado-11@Admin-11s-iMac.local>
2023-07-25 17:34:27 +0800

    add: welcome_page

--- wordfind_app/lib/welcome_page.dart
+++ wordfind_app/lib/welcome_page.dart
@@ -1,24 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:wordfind_app/gradiant_letter.dart';
 
+import 'gradiant_text.dart';
+
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      backgroundColor: Color(0xFFFBF5F2),
+      floatingActionButton: Container(
+        width: 310,
+        height: 60,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [
+                  Color(0xFFE86B02),
+                  Color(0xFFFA9541),
+                ]),
+            borderRadius: BorderRadius.circular(25)),
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25))),
+          child: Text("PLAY",
+              style: TextStyle(
+                fontFamily: "Nunito",
+                fontWeight: FontWeight.w700,
+                fontSize:
+              )),
+        ),
+      ),
+      body: Container(
         child: Container(
           child: Center(
             child: Column(
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [GradientLetter('W'),
-                  GradientLetter('O'),
-                  GradientLetter('R'),
-                  GradientLetter('D')],
-                )
+                  children: [
+                    GradientLetter('W'),
+                    GradientLetter('O'),
+                    GradientLetter('R'),
+                    GradientLetter('D')
+                  ],
+                ),
+                GradiantText("Game", 31.6),
+                Expanded(child: GradiantText("READY?", 25.0)),
               ],
             ),
           ),
